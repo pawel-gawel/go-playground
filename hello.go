a7b521c02c489ca1a48008bd38be3d05abb30973
Gadabout <gadabout.code@gmail.com>
2018-02-13 19:58:14 +0100

    variables declared in block with inferred type

--- hello.go
+++ hello.go
@@ -18,6 +18,8 @@ func main() {
 	var name, surname string = "Jan", "Kowalski"
 	name, surname = splitName("John Doe")
 	fmt.Printf("Name is %s and surname %s\n", name, surname)
+
+	variablesDeclaredInBlock()
 }
 
 func add(x, y int) (int, int, float32) {
@@ -28,4 +30,16 @@ func splitName(name string) (firstName, lastName string) {
 	firstName = strings.Split(name, " ")[0]
 	lastName = strings.Split(name, " ")[1]
 	return
+}
+
+func variablesDeclaredInBlock() {
+	var (
+		str string
+		bl  bool
+		a   int
+		b           = 1<<24 - 1
+		c   float32 = 7.4
+	)
+
+	fmt.Println(str, bl, a, b, c)
 }
