f8616587ebc95aead83b574540f39cb0e002ba39
Gadabout <gadabout.code@gmail.com>
2018-02-13 19:39:05 +0100

    sum with type casting

--- hello.go
+++ hello.go
@@ -6,8 +6,14 @@ import (
 	"math/rand"
 )
 
+func add(x, y int) float32 {
+	return float32(x + y)
+}
+
 func main() {
 	fmt.Printf("hello ziom!\n")
 	fmt.Println("Random number", rand.Intn(200))
 	fmt.Println("Sqrt", math.Sqrt(7))
+
+	fmt.Println("Add x + y = ", add(3, 4))
 }
