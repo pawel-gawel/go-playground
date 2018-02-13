83b06c84b0b7476d7a791cfac0c95b727d178f09
Gadabout <gadabout.code@gmail.com>
2018-02-13 19:36:10 +0100

    sqrt

--- hello.go
+++ hello.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 )
 
 func main() {
 	fmt.Printf("hello ziom!\n")
 	fmt.Println("Random number", rand.Intn(200))
+	fmt.Println("Sqrt", math.Sqrt(7))
 }
