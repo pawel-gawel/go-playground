73bc0400928136b9c13ecb585f516a59ffafad07
Gadabout <gadabout.code@gmail.com>
2018-02-19 13:59:44 +0100

    some slicing

--- slices.go
+++ slices.go
@@ -1,5 +1,17 @@
 package main
 
-func Slices() {
+import (
+	"fmt"
+)
 
+func Slices() {
+	someSlice()
+}
+
+func someSlice() {
+	a := [10]int{1, 2, 3, 4, 5}
+	var s []int
+	s = a[2:5]
+
+	fmt.Println("slice is ", s)
 }
