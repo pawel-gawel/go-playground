842d89b175529d11d3ff4be5806bc0be1c9fff9a
Gadabout <gadabout.code@gmail.com>
2018-02-19 19:57:53 +0100

    map literals

--- maps.go
+++ maps.go
@@ -8,12 +8,22 @@ func Maps() {
 	fmt.Println("\n----- MAPS -----\n")
 
 	makeMap()
+	mapLiterals()
 }
 
 func makeMap() {
 	var m map[string]int
 	m = make(map[string]int)
 	m["one"] = 1
+
+	fmt.Println(m)
+}
+
+func mapLiterals() {
+	m := map[string]int{
+		"one": 1,
+		"two": 2,
+	}
 
 	fmt.Println(m)
 }
